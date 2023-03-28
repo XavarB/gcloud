2c2988f13fe1a7a879e78ab117c99de8a7eab96f
Xavar B <zawarbashir321@gmail.com>
2023-03-29 02:39:02 +0500

    added

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ AdminJS.registerAdapter({
   Database: AdminJSMongoose.Database,
 })
 
-const PORT = process.env.PORT || 3004
+const PORT = process.env.PORT || 3000
 const DEFAULT_ADMIN = {
   email: 'admin@example.com',
   password: 'password',
